f063e0549fec66ac4920aa57f4f613edb3094176
lxl757266307 <757266307@qq.com>
2017-05-27 14:40:35 +0800

    one

--- app/src/main/java/com/example/hhhhhhhh/MainActivity.java
+++ app/src/main/java/com/example/hhhhhhhh/MainActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +16,8 @@ public class MainActivity extends Activity {
     }
 
 
-
+    @Override
+    public void onClick(View v) {
+        
+    }
 }
